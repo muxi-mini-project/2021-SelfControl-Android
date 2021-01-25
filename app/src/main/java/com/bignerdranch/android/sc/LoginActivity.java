4c2eb6def039502f39a8177f697920f1e40078c4
xiaozhu-sorce <790362227@qq.com>
2021-01-25 16:51:36 +0800

    LoginPage

--- app/src/main/java/com/bignerdranch/android/sc/LoginActivity.java
+++ app/src/main/java/com/bignerdranch/android/sc/LoginActivity.java
@@ -1,25 +1,37 @@
 package com.bignerdranch.android.sc;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.EditText;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
     private EditText musername;
     private EditText mpassword;
+    private Button mloginbutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_page);
+
+        mloginbutton=(Button)findViewById(R.id.login_B);
+        mloginbutton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(LoginActivity.this,StudyLabelActivity.class);
+                startActivity(intent);
+            }
+        });
 
         //设置状态栏透明
         makeStatusBarTransparent(this);
