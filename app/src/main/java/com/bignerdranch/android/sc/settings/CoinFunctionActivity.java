53938576c78b9b97094669a9e926c4966c37f8fa
liuyuxin-cloud <2590177089@qq.com>
2021-01-31 19:04:02 +0800

    setting

---
+++ app/src/main/java/com/bignerdranch/android/sc/settings/CoinFunctionActivity.java
@@ -0,0 +1,34 @@
+package com.bignerdranch.android.sc.settings;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.bignerdranch.android.sc.R;
+
+public class CoinFunctionActivity extends AppCompatActivity {
+
+    private ImageButton mBack;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        init();
+    }
+    private void init(){
+
+        mBack = (ImageButton)findViewById(R.id.coin_back);
+        mBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CoinFunctionActivity.this,SettingPageActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
