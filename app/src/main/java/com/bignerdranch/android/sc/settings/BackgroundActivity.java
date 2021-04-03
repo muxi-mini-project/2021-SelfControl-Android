a942ccdf3cd3ba71aa88e16d353170a3e4d58986
liuyuxin-cloud <2590177089@qq.com>
2021-04-03 23:45:00 +0800

    dbb

--- app/src/main/java/com/bignerdranch/android/sc/settings/BackgroundActivity.java
+++ app/src/main/java/com/bignerdranch/android/sc/settings/BackgroundActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.bignerdranch.android.sc.R;
 import com.bignerdranch.android.sc.StatusBar;
@@ -15,6 +16,8 @@ import com.bignerdranch.android.sc.user.UserClient;
 
 import java.util.List;
 
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -32,11 +35,10 @@ public class BackgroundActivity extends StatusBar {
     private ImageButton mBack;
     private ImageView mTheme1, mTheme2, mTheme3, mTheme4, mTheme5, mTheme6;
     private ImageView mChoose1, mChoose2, mChoose3, mChoose4, mChoose5, mChoose6;
-    private int f1 = 0, f2 = 0, f3 = 0, f4 = 0, f5 = 0, f6 = 0;
+    private int f1 = 1, f2 = 0, f3 = 0, f4 = 0, f5 = 0, f6 = 0;
+    private int b_1,b_2,b_3,b_4,b_5,b_6;
 
-    private User mUser;
-    private int mCoin;
-    private List<Background> mBackgroundList;
+    private myBackground mMyBackground;
 
     @Override
     protected void onCreate(Bundle SavedInstanceState) {
@@ -50,16 +52,25 @@ public class BackgroundActivity extends StatusBar {
     }
 
 
-
     private void init() {
 
-        mBack = (ImageButton) findViewById(R.id.background_back);
+        mBack = findViewById(R.id.background_back);
         mBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
             }
         });
+
+        mChoose1 = findViewById(R.id.choose1);
+        mChoose2 = findViewById(R.id.choose2);
+        mChoose3 = findViewById(R.id.choose3);
+        mChoose4 = findViewById(R.id.choose4);
+        mChoose5 = findViewById(R.id.choose5);
+        mChoose6 = findViewById(R.id.choose6);
+
+        request1();
+        updateView();
 
         mTheme1 = findViewById(R.id.theme1);
         mTheme1.setOnClickListener(new View.OnClickListener() {
@@ -80,76 +91,142 @@ public class BackgroundActivity extends StatusBar {
         mTheme2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                request();
 
-                f1 = 0;
-                f2 = 1;
-                f3 = 0;
-                f4 = 0;
-                f5 = 0;
-                f6 = 0;
+                request1();
+                if(b_2 == 0){
+                    showNormalDialog(3);
+                    request1();
+                    if(b_2 != 0 ){
+                        f1 = 0;
+                        f2 = 1;
+                        f3 = 0;
+                        f4 = 0;
+                        f5 = 0;
+                        f6 = 0;
+                    }
+                }else{
+
+                    f1 = 0;
+                    f2 = 1;
+                    f3 = 0;
+                    f4 = 0;
+                    f5 = 0;
+                    f6 = 0;
+                }
                 updateView();
+
             }
         });
         mTheme3 = findViewById(R.id.theme3);
         mTheme3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                f1 = 0;
-                f2 = 0;
-                f3 = 1;
-                f4 = 0;
-                f5 = 0;
-                f6 = 0;
+                request1();
+                if(b_3 == 0){
+                    showNormalDialog(3);
+                    request1();
+                    if(b_3 != 0 ){
+                        f1 = 0;
+                        f2 = 0;
+                        f3 = 1;
+                        f4 = 0;
+                        f5 = 0;
+                        f6 = 0;
+                    }
+                }else{
+
+                    f1 = 0;
+                    f2 = 0;
+                    f3 = 1;
+                    f4 = 0;
+                    f5 = 0;
+                    f6 = 0;
+                }
                 updateView();
             }
         });
-        mTheme4 = (ImageView) findViewById(R.id.theme4);
+        mTheme4 = findViewById(R.id.theme4);
         mTheme4.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                f1 = 0;
-                f2 = 0;
-                f3 = 0;
-                f4 = 1;
-                f5 = 0;
-                f6 = 0;
+            public void onClick(View v) {request1();
+                if(b_4 == 0){
+                    showNormalDialog(4);
+                    request1();
+                    if(b_4 != 0 ){
+                        f1 = 0;
+                        f2 = 0;
+                        f3 = 0;
+                        f4 = 1;
+                        f5 = 0;
+                        f6 = 0;
+                    }
+                }else{
+                    f1 = 0;
+                    f2 = 0;
+                    f3 = 0;
+                    f4 = 1;
+                    f5 = 0;
+                    f6 = 0;
+                }
                 updateView();
             }
         });
-        mTheme5 = (ImageView) findViewById(R.id.theme5);
+        mTheme5 = findViewById(R.id.theme5);
         mTheme5.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                f1 = 0;
-                f2 = 0;
-                f3 = 0;
-                f4 = 0;
-                f5 = 1;
-                f6 = 0;
+
+                if(b_5 == 0){
+                    showNormalDialog(5);
+                    request1();
+                    if(b_5 != 0 ){
+                        f1 = 0;
+                        f2 = 0;
+                        f3 = 0;
+                        f4 = 0;
+                        f5 = 1;
+                        f6 = 0;
+                    }
+                }else{
+                    f1 = 0;
+                    f2 = 0;
+                    f3 = 0;
+                    f4 = 0;
+                    f5 = 1;
+                    f6 = 0;
+                }
                 updateView();
             }
         });
-        mTheme6 = (ImageView) findViewById(R.id.theme6);
+        mTheme6 = findViewById(R.id.theme6);
         mTheme6.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                f1 = 0;
-                f2 = 0;
-                f3 = 0;
-                f4 = 0;
-                f5 = 0;
-                f6 = 1;
+                if(b_6 == 0){
+                    showNormalDialog(6);
+                    request1();
+                    if(b_6 != 0 ){
+                        f1 = 0;
+                        f2 = 0;
+                        f3 = 0;
+                        f4 = 0;
+                        f5 = 1;
+                        f6 = 0;
+                    }
+                }else{
+                    f1 = 0;
+                    f2 = 0;
+                    f3 = 0;
+                    f4 = 0;
+                    f5 = 1;
+                    f6 = 0;
+                }
+
                 updateView();
             }
         });
 
-        mChoose1 = findViewById(R.id.choose1);
-        mChoose2 = findViewById(R.id.choose2);
-        mChoose3 = findViewById(R.id.choose3);
-        mChoose4 = findViewById(R.id.choose4);
-        mChoose5 = findViewById(R.id.choose5);
-        mChoose6 = findViewById(R.id.choose6);
+
 
     }
 
@@ -194,11 +271,82 @@ public class BackgroundActivity extends StatusBar {
     }
 
     public interface BackgroundAPI {
-        @PUT
-        Call<Background> buyBackground(@Body String id, @Header("token") String token);
+        @PUT("backdrop/")
+        Call<Background> buyBackground(@Body Background mBackground, @Header("token") String token);
+    }
 
-        @GET
-        Call<List<Background>> myBackground(@Header("token") String token);
+    public interface myBackgroundAPI {
+
+        @GET("backdrops/")
+        Call<myBackground> getMyBackground(@Header("token") String token);
+    }
+
+    public class myBackground {
+
+        private int b_1;
+        private int b_2;
+        private int b_3;
+        private int b_4;
+        private int b_5;
+        private int b_6;
+
+        public myBackground(int b_1, int b_2, int b_3, int b_4, int b_5, int b_6) {
+            this.b_1 = b_1;
+            this.b_2 = b_2;
+            this.b_3 = b_3;
+            this.b_4 = b_4;
+            this.b_5 = b_5;
+            this.b_6 = b_6;
+        }
+
+        public void setB_1(int b_1) {
+            this.b_1 = b_1;
+        }
+
+        public int getB_1() {
+            return b_1;
+        }
+
+        public void setB_2(int b_2) {
+            this.b_2 = b_2;
+        }
+
+        public int getB_2() {
+            return b_2;
+        }
+
+        public void setB_3(int b_3) {
+            this.b_3 = b_3;
+        }
+
+        public int getB_3() {
+            return b_3;
+        }
+
+        public void setB_4(int b_4) {
+            this.b_4 = b_4;
+        }
+
+        public int getB_4() {
+            return b_4;
+        }
+
+        public void setB_5(int b_5) {
+            this.b_5 = b_5;
+        }
+
+        public int getB_5() {
+            return b_5;
+        }
+
+        public void setB_6(int b_6) {
+            this.b_6 = b_6;
+        }
+
+        public int getB_6() {
+            return b_6;
+        }
+
     }
 
     public class Background {
@@ -206,6 +354,10 @@ public class BackgroundActivity extends StatusBar {
         private int backdrop_id;
         private String picture_url;
         private int price;
+
+        public Background(int backdrop_id){
+            this.backdrop_id = backdrop_id;
+        }
 
         public void setBackdrop_id(int backdrop_id) {
             this.backdrop_id = backdrop_id;
@@ -233,7 +385,7 @@ public class BackgroundActivity extends StatusBar {
     }
 
 
-    private void showNormalDialog() {
+    private void showNormalDialog(int id) {
 
         final AlertDialog.Builder normalDialog = new AlertDialog.Builder(BackgroundActivity.this);
         normalDialog.setTitle("兑换背景");
@@ -241,6 +393,8 @@ public class BackgroundActivity extends StatusBar {
         normalDialog.setPositiveButton("确定", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
+
+                request2(id);
 
             }
         });
@@ -253,54 +407,82 @@ public class BackgroundActivity extends StatusBar {
         // 显示
         normalDialog.show();
     }
-    private void request(){
+
+    private void request1() {//获取拥有的背景
+
+        OkHttpClient.Builder okHttpClientBuilder = new OkHttpClient.Builder();
+        HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+                logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+                okHttpClientBuilder.addInterceptor(logging);
 
 
         Retrofit.Builder builder = new Retrofit.Builder()
                 .baseUrl("http://39.102.42.156:2333/api/v1/")
-                .addConverterFactory(GsonConverterFactory.create());
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(okHttpClientBuilder.build());
 
         Retrofit retrofit = builder.build();
-        BackgroundAPI client = retrofit.create(BackgroundAPI.class);
-        Call<List<Background>> call = client.myBackground(token);
+        myBackgroundAPI client = retrofit.create(myBackgroundAPI.class);
+        Call<myBackground> call = client.getMyBackground(token);
 
-        call.enqueue(new Callback<List<Background>>() {
+        call.enqueue(new Callback<myBackground>() {
 
             @Override
-            public void onResponse(Call<List<Background>> call, Response<List<Background>> response) {
-                mBackgroundList = response.body();
+            public void onResponse(Call<myBackground> call, Response<myBackground> response) {
+                b_1 = response.body().getB_1();
+                b_2 = response.body().getB_2();
+                b_3 = response.body().getB_3();
+                b_4 = response.body().getB_4();
+                b_5 = response.body().getB_5();
+                b_6 = response.body().getB_6();
+
             }
 
             @Override
-            public void onFailure(Call<List<Background>> call, Throwable t) {
+            public void onFailure(Call<myBackground> call, Throwable t) {
 
             }
-
         });
 
+    }
+
+    private void request2( int id) {   //兑换背景
+        /*OkHttpClient.Builder okHttpClientBuilder = new OkHttpClient.Builder();
+        HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+        okHttpClientBuilder.addInterceptor(logging);*/
 
         Retrofit.Builder builder1 = new Retrofit.Builder()
                 .baseUrl("http://39.102.42.156:2333/api/v1/")
                 .addConverterFactory(GsonConverterFactory.create());
 
-        Retrofit retrofit1 = builder.build();
-        UserClient client1 = retrofit.create(UserClient.class);
-        Call<User> call1 = client1.mUser(token);
+        Retrofit retrofit1 = builder1.build();
+        BackgroundAPI client1 = retrofit1.create(BackgroundAPI.class);
+        Call<Background> call1 = client1.buyBackground(new Background(id),token);
 
-        call1.enqueue(new Callback<User>() {
+        call1.enqueue(new Callback<Background>() {
 
             @Override
-            public void onResponse(Call<User> call, Response<User> response) {
-                mUser = response.body();
-                mCoin = mUser.getGold();
+            public void onResponse(Call<Background> call, Response<Background> response) {
+                if(response.code() == 200){
+                    Toast.makeText(BackgroundActivity.this,"兑换成功",Toast.LENGTH_SHORT).show();
+                }
+                else if(response.code() == 203){
+                    Toast.makeText(BackgroundActivity.this,"金币不足！快去打卡赚金币吧~",Toast.LENGTH_SHORT).show();
+                }
+                else if(response.code() == 400){
+                    Toast.makeText(BackgroundActivity.this,"出错啦！请稍后再试~",Toast.LENGTH_SHORT).show();
+                }else if(response.code() == 401){
+                    Toast.makeText(BackgroundActivity.this,"身份认证出错啦！",Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(BackgroundActivity.this,"出错啦！稍后再试~",Toast.LENGTH_SHORT).show();
+                }
             }
 
             @Override
-            public void onFailure(Call<User> call, Throwable t) {
-
+            public void onFailure(Call<Background> call, Throwable t) {
+                Toast.makeText(BackgroundActivity.this,"出错啦！请检查网络连接~",Toast.LENGTH_SHORT).show();
             }
         });
-
-
     }
 }
