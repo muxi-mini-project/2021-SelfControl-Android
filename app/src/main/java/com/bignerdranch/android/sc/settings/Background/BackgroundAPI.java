6d89bb5372eb2b34644a5739c1bdd12dd64972d2
刘昱欣 <2590177089@qq.com>
2021-08-01 22:02:17 +0800

    api

--- app/src/main/java/com/bignerdranch/android/sc/settings/Background/BackgroundAPI.java
+++ app/src/main/java/com/bignerdranch/android/sc/settings/Background/BackgroundAPI.java
@@ -1,5 +1,12 @@
 package com.bignerdranch.android.sc.settings.Background;
 
+
+import com.bignerdranch.android.sc.settings.Background.Items.myBackground1;
+
+import io.reactivex.Observable;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+
 public interface BackgroundAPI {
     interface M{
         void changeBackground();
@@ -11,4 +18,9 @@ public interface BackgroundAPI {
         void error();
 
     }
+    @GET("/backdrops")
+    Observable<myBackground1> getMyBack(@Header("token") String token);
+
+    @GET("/backdrop")
+    Observable<Items.Background1> getPrice(@Header("token") String token);
 }
