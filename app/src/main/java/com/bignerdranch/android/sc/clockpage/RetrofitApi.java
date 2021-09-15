a51ad7519c5390eaefc4c890b4a543d798ba2d67
xiaozhu-sorce <790362227@qq.com>
2021-09-15 23:54:18 +0800

    fix:name

--- app/src/main/java/com/bignerdranch/android/sc/clockpage/RetrofitApi.java
+++ app/src/main/java/com/bignerdranch/android/sc/clockpage/RetrofitApi.java
@@ -1,16 +1,11 @@
 package com.bignerdranch.android.sc.clockpage;
 
 import com.bignerdranch.android.sc.clockpage.model.FlowerResponse;
-import com.bignerdranch.android.sc.punch.LabelPunch;
 
-import java.util.List;
-
-import io.reactivex.Observable;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
 import retrofit2.http.Path;
-import retrofit2.http.Query;
 
 public interface RetrofitApi {
 
