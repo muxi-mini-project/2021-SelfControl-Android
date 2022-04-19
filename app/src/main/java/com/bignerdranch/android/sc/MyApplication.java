baa5d45250f7a4366c718421a6fdd7f26e5fcc7e
刘昱欣 <2590177089@qq.com>
2022-04-19 21:43:02 +0800

    touxiang

---
+++ app/src/main/java/com/bignerdranch/android/sc/MyApplication.java
@@ -0,0 +1,13 @@
+package com.bignerdranch.android.sc;
+
+import android.app.Application;
+
+import com.facebook.drawee.backends.pipeline.Fresco;
+
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Fresco.initialize(this);
+    }
+}
